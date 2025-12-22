74f78a7a78f934d99a1f053153ff9c49963120c3
jiangyuntao <jiangyuntao@gmail.com>
2025-12-23 00:16:49 +0800

    feat(models): 实现FilamentUser接口以支持Filament面板

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -9,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasRoles;
 
