84167ca8cd96eb408fe7b7af3722fe1dd9a9a646
jiangyuntao <jiangyuntao@gmail.com>
2025-12-22 23:57:42 +0800

    feat(首页): 实现首页路由、控制器和视图
    添加IndexController处理首页逻辑
    创建BaseController作为基础控制器
    实现完整的首页视图模板

---
+++ src/app/Http/Controllers/Index/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Index;
+
+use Illuminate\Http\Request;
+
+class IndexController extends BaseController
+{
+    public function index(Request $request)
+    {
+        return view('index.index.index', $this->data);
+    }
+}
