743d8317476275528316370d795398f1838bf747
jiangyuntao <jiangyuntao@gmail.com>
2026-01-15 22:28:45 +0800

    feat(页面布局): 更新生产流程和关于我们页面的响应式设计和图片布局
    refactor(视图文件): 优化网格布局和间距，调整图片路径和显示方式
    style(样式): 统一字体大小和颜色，改进视觉层次结构

--- src/resources/views/index/page/show.blade.php
+++ src/resources/views/index/page/show.blade.php
@@ -26,22 +26,29 @@
 @section('main')
   <main class="max-w-[1200px] mx-auto  px-6 py-12 md:py-20 font-serif">
     <section class="w-full mb-16">
-      <video class="inset-0 w-full h-full object-cover shadow-2xl" autoplay loop muted playsinline>
-        <source src="{{ asset('videos/about-us-top.mp4') }}" type="video/mp4">
+      <video class="inset-0 w-full h-full object-cover shadow-2xl"
+             autoplay
+             loop
+             muted
+             playsinline>
+        <source src="{{ asset('videos/about-us-top.mp4') }}"
+                type="video/mp4">
         Your browser does not support the video tag.
       </video>
     </section>
 
-    <section class="grid grid-cols-1 md:grid-cols-12 gap-12 mb-16 items-start">
-      <div class="md:col-span-6 flex flex-col items-start">
+    <section class="grid grid-cols-1 md:grid-cols-12 gap-6 mb-16 items-start">
+      <div class="md:col-span-5 flex flex-col items-start">
         <div class="mb-4">
           <div class="flex items-center gap-2 pt-8">
-            <img src="{{ asset('images/start-2.webp') }}" alt="Yierli Logo" class="h-60 w-auto object-contain">
+            <img src="{{ asset('images/start-2.webp') }}"
+                 alt="Yierli Logo"
+                 class="h-60 w-auto object-contain">
           </div>
         </div>
       </div>
 
-      <div class="md:col-span-6">
+      <div class="md:col-span-7">
         <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-12">Heritage & Commitment</h2>
         <div class="space-y-4 text-sm md:text-lg leading-relaxed text-gray-800">
           <p>
@@ -59,8 +66,9 @@
 
     <section class="w-full mb-24">
       <div class="relative w-full h-64 md:h-96 bg-gray-300 overflow-hidden rounded-sm shadow-sm">
-        <img src="{{ asset('images/about-us/Heritage-Commitment.png') }}" alt="Factory Building"
-          class="w-full h-full object-cover opacity-90">
+        <img src="{{ asset('images/about-us/Heritage-Commitment.png') }}"
+             alt="Factory Building"
+             class="w-full h-full object-cover opacity-90">
         <div class="absolute inset-0 bg-white/20"></div>
       </div>
     </section>
@@ -84,49 +92,57 @@
         </div>
       </div>
 
-      <div class="order-1 md:col-span-5 md:order-2 h-64 md:h-auto rounded-sm shadow-sm pt-24">
+      <div class="order-1 md:col-span-5 md:order-2 h-64 md:h-auto rounded-sm shadow-sm pt-16">
         <div class="grid grid-cols-2 gap-3">
-          <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-            <img src="{{ asset('images/about-us/Integrated-Manufacturing-1.jpg') }}" class="w-full h-full object-cover">
+          <div class="aspect-[16/10] bg-[#367C6D] rounded-sm">
+            <img src="{{ asset('images/about-us/Integrated-Manufacturing-1.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
-          <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-            <img src="{{ asset('images/about-us/Integrated-Manufacturing-2.jpg') }}" class="w-full h-full object-cover">
+          <div class="aspect-[16/10] bg-[#367C6D] rounded-sm">
+            <img src="{{ asset('images/about-us/Integrated-Manufacturing-2.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
-          <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
+          <div class="aspect-[16/10] bg-[#367C6D] rounded-sm">
             <img src="{{ asset('images/about-us/Integrated-Manufacturing-3.jpg') }}"
-              class="w-full h-full object-cover">
+                 class="w-full h-full object-cover">
           </div>
-          <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
+          <div class="aspect-[16/10] bg-[#367C6D] rounded-sm">
             <img src="{{ asset('images/about-us/Integrated-Manufacturing-4.jpg') }}"
-              class="w-full h-full object-cover">
+                 class="w-full h-full object-cover">
           </div>
         </div>
       </div>
     </section>
 
     <section class="grid grid-cols-1 md:grid-cols-12 gap-12 mb-24 items-center">
-      <div class="grid grid-cols-2 md:col-span-7 gap-2 pr-28">
+      <div class="grid grid-cols-2 md:col-span-6 gap-3">
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-5.jpg') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-5.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-6.jpg') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-6.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-7.jpg') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-7.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-8.jpg') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-8.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-9.jpg') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-9.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="rounded-sm overflow-hidden aspect-[16/9]">
-          <img src="{{ asset('images/about-us/Integrated-Manufacturing-10.png') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Integrated-Manufacturing-10.png') }}"
+               class="w-full h-full object-cover">
         </div>
       </div>
-      <div class="md:col-span-5">
-        <ul class="space-y-4 text-[#1F5F53] font-medium text-sm md:text-lg">
+      <div class="md:col-span-6">
+        <ul class="space-y-4 text-[#1F5F53] font-medium text-sm md:text-base pl-24">
           <li class="flex items-center gap-2">
             <span class="w-1.5 h-1.5 bg-[#1F5F53] rounded-full"></span>
             Germany Heidelberg four-color printing machine
@@ -170,7 +186,7 @@
         </ul>
       </div>
     </section>
-    <section class="mb-20">
+    <section class="mb-12">
       <div class="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-12">
         <div class="md:col-span-7">
           <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-6">Quality & Compliance</h2>
@@ -185,72 +201,81 @@
         </div>
         <div class="md:col-span-5 grid grid-cols-2 gap-3">
           <div class="aspect-[16/9] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Quality-Compliance-1.jpg') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Quality-Compliance-1.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
           <div class="aspect-[16/9] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Quality-Compliance-2.jpg') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Quality-Compliance-2.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
           <div class="aspect-[16/9] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Quality-Compliance-3.jpg') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Quality-Compliance-3.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
           <div class="aspect-[16/9] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Quality-Compliance-4.jpg') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Quality-Compliance-4.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
         </div>
       </div>
 
-      <div class="mb-16">
+      <div class="mb-6">
         <div class="flex flex-col items-center">
           <div class="w-full overflow-hidden relative group">
 
             <div class="flex gap-8 w-max animate-marquee group-hover:pause">
 
               @for ($i = 0; $i < 3; $i++)
-              @for ($j = 1; $j <= 8; $j++)
-              <img src="{{ asset('images/slides/' . $j . '.webp') }}"
-                class="h-64 w-auto shadow-sm rounded-sm">
-              @endfor
+                @for ($j = 1; $j <= 8; $j++)
+                  <img src="{{ asset('images/slides/' . $j . '.webp') }}"
+                       class="h-64 w-auto shadow-sm rounded-sm">
+                @endfor
               @endfor
             </div>
 
             <div
-              class="absolute top-0 start-0 w-16 h-full bg-gradient-to-r from-yideli-base to-transparent pointer-events-none">
+                 class="absolute top-0 start-0 w-16 h-full bg-gradient-to-r from-yideli-base to-transparent pointer-events-none">
             </div>
             <div
-              class="absolute top-0 end-0 w-16 h-full bg-gradient-to-l from-yideli-base to-transparent pointer-events-none">
+                 class="absolute top-0 end-0 w-16 h-full bg-gradient-to-l from-yideli-base to-transparent pointer-events-none">
             </div>
           </div>
         </div>
 
-        <div class="w-full max-w-md mx-auto h-px bg-[#A8C5BD] mt-8 text-center relative">
-          <div class="absolute end-0 -top-1 bg-[url('{{ asset('images/line.jpg') }}')] rounded-full"></div>
+        <div class="flex justify-end w-full">
+          <img src="{{ asset('images/line.jpg') }}"
+            alt="separator"
+            class="w-full md:w-1/3 h-auto" />
         </div>
       </div>
     </section>
 
-
-    <section class="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-20">
+    <section class="grid grid-cols-1 md:grid-cols-12 gap-24 items-center mb-20">
       <div class="md:col-span-6 order-2 md:order-1">
         <div class="flex flex-col gap-4">
-          <div class="grid grid-cols-2 gap-4">
-            <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-              <img src="{{ asset('images/about-us/Global-Reach-1.webp') }}" class="w-full h-full object-fit">
+          <div class="grid grid-cols-2 gap-0">
+            <div class="aspect-[4/3] bg-[#367C6D] rounded-sm mb-2">
+              <img src="{{ asset('images/about-us/Global-Reach-1.jpg') }}"
+                   class="w-full h-full object-fit">
             </div>
-            <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-              <img src="{{ asset('images/about-us/Global-Reach-2.webp') }}" class="w-full h-full object-fit">
+            <div class="aspect-[4/3] bg-[#367C6D] rounded-sm mb-1">
+              <img src="{{ asset('images/about-us/Global-Reach-2.jpg') }}"
+                   class="w-full h-full object-fit">
             </div>
-            <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-              <img src="{{ asset('images/about-us/Global-Reach-3.webp') }}" class="w-full h-full object-fit">
+            <div class="aspect-[4/3] bg-[#367C6D] rounded-sm">
+              <img src="{{ asset('images/about-us/Global-Reach-3.jpg') }}"
+                   class="w-full h-full object-fit">
             </div>
-            <div class="aspect-[16/9] bg-[#367C6D] rounded-sm">
-              <img src="{{ asset('images/about-us/Global-Reach-4.webp') }}" class="w-full h-full object-fit">
+            <div class="aspect-[4/3] bg-[#367C6D] rounded-sm">
+              <img src="{{ asset('images/about-us/Global-Reach-4.jpg') }}"
+                   class="w-full h-full object-fit">
             </div>
           </div>
         </div>
       </div>
 
       <div class="md:col-span-6 order-1 md:order-2">
-        <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-6">Global Reach</h2>
+        <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-12">Global Reach</h2>
         <div class="text-sm md:text-lg leading-relaxed text-gray-800 space-y-4">
           <p>
             Guided by our core values of Integrity, Focus, Quality, and Efficiency, we combine innovative design
@@ -265,9 +290,9 @@
       </div>
     </section>
 
-    <section class="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-20">
+    <section class="grid grid-cols-1 md:grid-cols-12 gap-24 items-center mb-20">
       <div class="md:col-span-6">
-        <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-6">R&D & Innovation</h2>
+        <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-12">R&D & Innovation</h2>
         <div class="text-sm md:text-lg leading-relaxed text-gray-800 space-y-4">
           <p>
             To fuel continuous innovation, we maintain a dedicated R&D center in Hangzhou.
@@ -278,36 +303,44 @@
             market trends.
           </p>
         </div>
-        <div class="w-full h-px bg-[#A8C5BD] mt-8 relative">
+        <div class="flex justify-end w-full py-4">
+          <img src="{{ asset('images/line.jpg') }}"
+            alt="separator"
+            class="w-full md:w-2/3 h-auto" />
         </div>
       </div>
 
-      <div class="md:col-span-6 grid grid-cols-2 gap-4">
-        <img src="{{ asset('images/about-us/RandD-Innovation-1.webp') }}" alt="R&D Team Member"
-          class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
-        <img src="{{ asset('images/about-us/RandD-Innovation-2.webp') }}" alt="Office Environment"
-          class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
-        <img src="{{ asset('images/about-us/RandD-Innovation-3.webp') }}" alt="R&D Team Member"
-          class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
-        <img src="{{ asset('images/about-us/RandD-Innovation-4.webp') }}" alt="Office Environment"
-          class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
+      <div class="md:col-span-6 grid grid-cols-2 gap-3">
+        <img src="{{ asset('images/about-us/RandD-Innovation-1.jpg') }}"
+             alt="R&D Team Member"
+             class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
+        <img src="{{ asset('images/about-us/RandD-Innovation-2.jpg') }}"
+             alt="Office Environment"
+             class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
+        <img src="{{ asset('images/about-us/RandD-Innovation-3.jpg') }}"
+             alt="R&D Team Member"
+             class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
+        <img src="{{ asset('images/about-us/RandD-Innovation-4.jpg') }}"
+             alt="Office Environment"
+             class="w-full aspect-[1/1] object-cover rounded-sm shadow-sm">
       </div>
     </section>
 
-
     <section class="mb-12">
-      <div class="grid grid-cols-1 md:grid-cols-12 gap-12 items-center mb-12">
+      <div class="grid grid-cols-1 md:grid-cols-12 gap-24 items-center mb-3">
         <div class="md:col-span-6 order-2 md:order-1 grid grid-cols-1 gap-3">
           <div class="aspect-[32/12] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Your-Strategic-Partner-1.webp') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Your-Strategic-Partner-1.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
           <div class="aspect-[32/12] bg-[#367C6D] rounded-sm opacity-90">
-            <img src="{{ asset('images/about-us/Your-Strategic-Partner-2.webp') }}" class="w-full h-full object-cover">
+            <img src="{{ asset('images/about-us/Your-Strategic-Partner-2.jpg') }}"
+                 class="w-full h-full object-cover">
           </div>
         </div>
 
         <div class="md:col-span-6 order-1 md:order-2">
-          <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-6">Your Strategic Partner</h2>
+          <h2 class="text-2xl md:text-4xl font-black text-[#1F5F53] mb-12">Your Strategic Partner</h2>
           <div class="text-sm md:text-lg leading-relaxed text-gray-800 space-y-4">
             <p>
               We position ourselves as more than a supplier; we are a strategic partner in the notebook
@@ -322,52 +355,27 @@
         </div>
       </div>
 
-      <div class="md:col-span-12 order-1 md:order-3 grid grid-cols-3 gap-3">
+      <div class="md:col-span-12 order-1 md:order-3 grid grid-cols-4 gap-3">
         <div class="aspect-[2/3] bg-[#367C6D] rounded-sm opacity-90">
-          <img src="{{ asset('images/about-us/Your-Strategic-Partner-3.webp') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Your-Strategic-Partner-3.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="aspect-[2/3] bg-[#367C6D] rounded-sm opacity-90">
-          <img src="{{ asset('images/about-us/Your-Strategic-Partner-4.webp') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Your-Strategic-Partner-4.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
         <div class="aspect-[2/3] bg-[#367C6D] rounded-sm opacity-90">
-          <img src="{{ asset('images/about-us/Your-Strategic-Partner-5.webp') }}" class="w-full h-full object-cover">
+          <img src="{{ asset('images/about-us/Your-Strategic-Partner-5.jpg') }}"
+               class="w-full h-full object-cover">
         </div>
+        <div></div>
       </div>
     </section>
 
     <section>
-      <img src="{{ asset('images/about-us/Your-Strategic-Partner-7.webp') }}" class="w-full h-full object-cover">
+      <img src="{{ asset('images/about-us/Your-Strategic-Partner-7.png') }}"
+           class="w-full h-full object-cover">
     </section>
   </main>
-  {{-- <section class="relative">
-    <img src="{{ asset('images/about-us-1.jpg') }}" class="w-full object-cover" alt="Design Process">
-  </section>
-  --}}
-
-  {{-- <section class="bg-yideli-dark text-yideli-base py-16">
-    <div
-      class="max-w-[1600px] mx-auto px-6 lg:px-12 grid grid-cols-2 md:grid-cols-4 gap-8 text-center divide-x divide-white/10">
-      <div>
-        <span class="block text-4xl lg:text-5xl font-serif font-bold mb-2">2005</span>
-        <span class="text-sm uppercase tracking-widest opacity-80">Established</span>
-      </div>
-      <div>
-        <span class="block text-4xl lg:text-5xl font-serif font-bold mb-2">10k+</span>
-        <span class="text-sm uppercase tracking-widest opacity-80">Square Meters</span>
-      </div>
-      <div>
-        <span class="block text-4xl lg:text-5xl font-serif font-bold mb-2">50+</span>
-        <span class="text-sm uppercase tracking-widest opacity-80">Export Countries</span>
-      </div>
-      <div>
-        <span class="block text-4xl lg:text-5xl font-serif font-bold mb-2">ISO</span>
-        <span class="text-sm uppercase tracking-widest opacity-80">Certified Factory</span>
-      </div>
-    </div>
-  </section>
-
-  <section class="relative">
-    <img src="{{ asset('images/about-us-2.jpg') }}" class="w-full object-cover" alt="Design Process">
-  </section> --}}
 
 @endsection
--- src/resources/views/index/production-process/index.blade.php
+++ src/resources/views/index/production-process/index.blade.php
@@ -1,11 +1,11 @@
 @extends('index.layout')
 
 @section('main')
-  <div class="relative bg-yideli-base py-24 lg:py-32 border-b border-yideli-line overflow-hidden">
+  <div class="relative bg-yideli-base py-12 md:py-12 overflow-hidden">
     <div class="absolute top-0 end-0 w-1/3 h-full bg-yideli-dark/5 skew-x-12 translate-x-1/2"></div>
 
-    <div class="max-w-[1600px] mx-auto px-6 lg:px-12 text-center relative z-10">
-      <h1 class="text-4xl lg:text-6xl font-serif text-yideli-dark mb-6">Customize from Concept</h1>
+    <div class="max-w-[1600px] mx-auto px-6 md:px-12 text-center relative z-10">
+      <h1 class="text-4xl md:text-5xl font-serif font-bold text-yideli-dark mb-6">Customize from Concept</h1>
       <p class="text-gray-600 max-w-2xl mx-auto space-y-4 text-sm md:text-lg leading-relaxed text-gray-800">
         We provide a clear path to realization your customize require . From executing your supplied designs to offering
         our own
@@ -17,12 +17,11 @@
     </div>
   </div>
 
-  <section class="max-w-[1600px] mx-auto px-6 lg:px-12 py-20 lg:py-32 space-y-24">
+  <section class="max-w-[1200px] mx-auto px-6 md:px-12 py-12 md:py-12 space-y-24">
 
-    <div class="grid lg:grid-cols-2 gap-12 lg:gap-20 items-center">
-      <div class="order-2 lg:order-1 relative">
-        <div class="step-number text-yideli-dark">01</div>
-        <h2 class="text-4xl font-serif text-yideli-dark mb-4 relative z-10">Design Co-creation</h2>
+    <div class="grid md:grid-cols-12 gap-12 md:gap-20 items-center">
+      <div class="order-2 md:col-span-7 md:order-1 relative">
+        <h2 class="text-4xl font-serif font-bold text-yideli-dark mb-4 relative z-10">Design Co-creation</h2>
         <p class="text-gray-800 text-lg leading-relaxed relative z-10">
           It all begins with the inspiration. You can provide complete designs for OEM production, or select and adapt
           from our extensive existing design library. Our design team works closely with you to professionally refine
@@ -32,20 +31,19 @@
           needs.
         </p>
       </div>
-      <div class="order-1 lg:order-2">
-        <img src="{{ asset('images/process-1.png') }}" class="w-auto h-[300px] object-cover rounded-sm shadow-xl"
+      <div class="order-1 md:col-span-5 md:order-2">
+        <img src="{{ asset('images/process/1.jpg') }}" class="w-full h-full object-cover rounded-sm shadow-xl"
           alt="Design Process">
       </div>
     </div>
 
-    <div class="grid lg:grid-cols-2 gap-12 lg:gap-20 items-center">
-      <div class="order-1">
-        <img src="{{ asset('images/process-2.png') }}" class="w-auto h-[300px] object-cover rounded-sm shadow-xl"
-          alt="Raw Material">
+    <div class="grid md:grid-cols-12 gap-12 md:gap-20 items-center">
+      <div class="order-1 md:col-span-5 md:grid md:grid-cols-2 md:gap-4">
+        <img src="{{ asset('images/process/2.png') }}" class="w-full h-full object-cover rounded-sm shadow-xl">
+        <img src="{{ asset('images/process/3.png') }}" class="w-full h-full object-cover rounded-sm shadow-xl">
       </div>
-      <div class="order-2 relative">
-        <div class="step-number text-yideli-dark end-0 start-auto">02</div>
-        <h2 class="text-4xl font-serif text-yideli-dark mb-4 relative z-10">Material Selection</h2>
+      <div class="order-2 md:col-span-7 relative">
+        <h2 class="text-4xl font-serif font-bold text-yideli-dark mb-4 relative z-10 md:text-right">Material Selection</h2>
         <p class="text-gray-800 text-lg leading-relaxed relative z-10">
           Quality begins with precise control over raw materials. Leveraging long-term partnerships with trusted
           suppliers, we
@@ -56,31 +54,30 @@
       </div>
     </div>
 
-    <div class="grid lg:grid-cols-2 gap-12 lg:gap-20 items-center">
-      <div class="order-2 relative">
-        <div class="step-number text-yideli-dark end-0 start-auto">03</div>
-        <h2 class="text-4xl font-serif text-yideli-dark mb-4 relative z-10">Sample Approval</h2>
+    <div class="grid md:grid-cols-12 gap-12 md:gap-20 items-center">
+      <div class="order-2 relative md:col-span-7">
+        <h2 class="text-4xl font-serif font-bold text-yideli-dark mb-4 relative z-10">Sample Approval</h2>
         <p class="text-gray-800 text-lg leading-relaxed relative z-10">
           After design finalization, we produce an accurate physical sample for your final confirmation. This critical
           step allows you to personally experience and approve every detail, guaranteeing that mass production will adhere
           exactly to the sample's standards.
         </p>
       </div>
-      <div class="order-1 lg:order-2 relative">
-        <img src="{{ asset('images/process-3.png') }}" class="w-auto h-[300px] object-cover rounded-sm shadow-xl"
+      <div class="order-1 md:col-span-5 md:order-2 relative">
+        <img src="{{ asset('images/process/4.png') }}" class="w-full h-full object-cover rounded-sm shadow-xl"
           alt="Raw Material">
       </div>
     </div>
 
   </section>
 
-  <section class="bg-yideli-dark text-white py-10 lg:py-18">
-    <div class="max-w-[1600px] mx-auto px-6 lg:px-12 text-center">
-      <h2 class="text-3xl lg:text-4xl font-serif">Custom Proposal</h2>
+  <section class="bg-yideli-dark text-white py-10 md:py-18">
+    <div class="max-w-[1600px] mx-auto px-6 md:px-12 text-center">
+      <h2 class="text-3xl md:text-4xl font-serif">Custom Proposal</h2>
     </div>
   </section>
 
-  <section class="px-6 lg:px-12 bg-[#f6fff4]">
+  <section class="px-6 md:px-12 bg-[#f6fff4]">
     <div class="max-w-[1600px] mx-auto">
       <img src="{{ asset('images/custom-1-big.jpg') }}">
     </div>
