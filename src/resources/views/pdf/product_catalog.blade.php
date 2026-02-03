461ae4e4233673befe6468f00c7f8d3366d3eca1
姜汁 <7105434@qq.com>
2026-02-03 17:58:45 +0800

    feat(pdf): 添加中文字体以支持中文PDF生成
    添加 Noto Sans SC 字体文件并更新产品目录PDF视图
    引入 Helvetica 和 Helvetica-Bold 字体度量文件
    确保中文环境下使用正确的字体渲染

--- src/resources/views/pdf/product_catalog.blade.php
+++ src/resources/views/pdf/product_catalog.blade.php
@@ -17,7 +17,10 @@
     @if($locale == 'zh')
       /* 中文：使用支持中文的字体 */
       body {
-        font-family: 'firefly', 'msyh', 'DejaVu Sans', sans-serif;
+        font-family: 'Noto Sans SC', 'msyh', 'DejaVu Sans', sans-serif;
+        src: url('{{ storage_path('fonts/NotoSansSC-Regular.ttf') }}') format('truetype');
+        font-weight: normal;
+        font-style: normal;
       }
 
     @elseif($locale == 'ru')
